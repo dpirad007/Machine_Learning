a09c571e0980a4968b4bdd3047b34dbccf5aa747
Dion Pinto <dionpinto000@gmail.com>
2020-04-16 10:52:45 +0530

    Added new regressions

---
+++ Regression-SVR-randomfor-decisiontree/decision_tree_regression_dion.py
@@ -0,0 +1,52 @@
+#decision tree regression 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values#when we add only [:,1] => we get a vector to solve issue we use [:,1:2](same thing but rep as a matrix now)
+y = dataset.iloc[:, 2:3].values
+
+# =============================================================================
+# # Splitting the dataset into the Training set and Test set
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# =============================================================================
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))"""
+
+
+#fitting the regression 
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
+
+
+
+#create your regressor
+
+
+#predicting a new result with Decision tree regression
+y_pred = regressor.predict([[6.5]])
+
+
+
+#visualising the regression results (for higher resultion and smooter curve)
+X_grid = np.arange(min(X),max(X),0.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or bluff (Decision tree Regression)')
+plt.xlabel('Postion level')
+plt.ylabel('Salary')
+plt.show()
+
